11f547ba80332e60f12b037e0f17cdcc0a59d7ee
Cristopher Benavides <rashier13@gmail.com>
2022-09-27 23:43:24 +0200

    ✨ App woking done

--- main.js
+++ main.js
@@ -3,13 +3,12 @@ import './style.css'
 import { counter } from './text-counter.js'
 
 
-let text = document.getElementById('text')
 
 document.querySelector('#app').innerHTML = `
-  <div className="container">
-    <div className="title">Text Counter</div>
+  <div class="container">
+    <div class="title">Text Counter</div>
     <textarea id="text" placeholder="Type your text here..."></textarea>
-    <div className="status">
+    <div class="status">
       <span id="char"></span>
       <span id="words"></span>
       <span id="lines"></span>
@@ -17,5 +16,6 @@ document.querySelector('#app').innerHTML = `
     </div>
   </div>
 `
+let text = document.getElementById('text')
 
 text.addEventListener('input', counter)
--- text-counter.js
+++ text-counter.js
@@ -1,21 +1,26 @@
+
 export function counter() {
   let stats = document.querySelector('.status')
   let text = document.getElementById('text')
-  let char = document.getElementById('charr')
+  let char = document.getElementById('char')
   let words = document.getElementById('words')
   let lines = document.getElementById('lines')
   let symbols = document.getElementById('symbols')
-  
+  let o = 'a'
+  console.log(o);
+  console.log(o.split(/[!@#$%^&*+()_={};:'"<>.,?/-]/).length);
+
+
   if (text.value.length === 0) {
     stats.style.display = 'none'
   } else {
     stats.style.display = 'block'
+    char.innerHTML = text.value.length + ' Characters'
+    words.innerHTML = text.value.trim().split(/\s+/).length + ' Words'
+    lines.innerHTML = text.value.split('\n').length + ' Lines'
+    symbols.innerHTML = text.value.split(/[!@#$%^&*+()_={};:'"<>.,?/-]/).length - 1 + ' Symbols'
   }
 
-  char.innerHTML = text.value.length + ' Characters'
-  words.innerHTML = text.value.trim().split(/\s+/).length + ' Words'
-  lines.innerHTML = text.value.split('\n').length + ' Lines'
-  symbols.innerHTML = text.value.split(/[!@#$%^&*+()_={};:'"<>.,?/-]/).length + ' Symbols'
 
-  
 }
+
